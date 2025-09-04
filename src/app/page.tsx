b9766c9d21edb08dbdb3309387fc00209708aa68
VinodAlexRaj <vinodalexraj@gmail.com>
2025-09-04 16:03:58 +0800

    3rd Fix [uid]/page.tsx export for Vercel build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,12 @@ export default async function HomePage() {
   const client = createClient();
   const page = await client.getByUID("homepage", "homepage");
 
+  // Use the actual field from Prismic
+  const mainTitle = page.data.title ?? page.data.heading ?? "Welcome";
+
   return (
     <main>
-      <h1>{page.data.title}</h1>
+      <h1>{mainTitle}</h1>
       <SliceZone slices={page.data.slice_zone} components={components} />
     </main>
   );
